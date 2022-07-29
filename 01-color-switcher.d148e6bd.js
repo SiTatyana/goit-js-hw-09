31b61b6eae05435db4a68fc50ede8097148224fd
SiTatyana <97842688+SiTatyana@users.noreply.github.com>
2022-07-29 13:41:14 +0000

    Deploying to gh-pages from @ SiTatyana/goit-js-hw-09@4691c1c7d5b007562ba901bb45658d8c3caa07d5 🚀

---
+++ 01-color-switcher.d148e6bd.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),o=document.querySelector("body");let r=null;t.addEventListener("click",(()=>{r=setInterval((()=>{o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.disabled=!0})),e.addEventListener("click",(()=>{clearInterval(r),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.d148e6bd.js.map
