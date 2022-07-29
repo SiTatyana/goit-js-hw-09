4691c1c7d5b007562ba901bb45658d8c3caa07d5
SiTatyana <sisenorawork@gmail.com>
2022-07-29 14:40:37 +0100

    finish task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const startColorSwitcher = document.querySelector('button[data-start]');
+const stopColorSwitcher = document.querySelector('button[data-stop]');
+const body = document.querySelector("body");
 
+let timerId = null;
+
+const disableButton = () => {
+    startColorSwitcher.disabled = true;
+};
+
+const undisableButton = () => {
+    startColorSwitcher.disabled = false;
+};
+
+startColorSwitcher.addEventListener("click", () => {
+  timerId = setInterval(() => {
+   changeColor() ;
+  }, 1000);
+    disableButton();
+});
+
+stopColorSwitcher.addEventListener("click", () => {
+    clearInterval(timerId);
+     undisableButton();
+});
+
+function changeColor(event) {
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
